451b7475b523fbd2a28f19c5957cfb2d2943f083
kirk-nez <gwenpool@protonmail.com>
2024-04-05 00:23:41 +0800

    first commit

---
+++ load.js
@@ -0,0 +1,5 @@
+navHTML = "<a href='/Pages/Bookbug.html'>Library</a>\
+<a href='/Pages/Dining.html'>Dining Hall</a>\
+<a href='/Pages/Aboutme.html'>Lore Room</a>"
+
+document.getElementById('nav').innerHTML = navHTML
